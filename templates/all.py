22a6d94b1ccafac729fe045620786582d6cb5b15
hugh <xiao.he@cfgglobal.co.nz>
2018-02-04 12:10:54 +1300

    alter template

--- templates/all.py
+++ templates/all.py
@@ -2,8 +2,9 @@ class {{modelFields.entity_name[0]}}Filter(FilterSet):
     class Meta:
         model = {{modelFields.entity_name[0]}}
         fields = { {% for var in modelFields.child_vars[1]%}
-            '{{var}}': ['exact', 'in', 'startswith'], {% endfor %}{% for var in modelFields.child_vars[2]%}
-            '{{var}}': ['exact', 'lt', 'gt'], {% endfor %}}
+            '{{var}}': ['exact', 'in', 'startswith', 'contains'], {% endfor %}{% for var in modelFields.child_vars[2]%}
+            '{{var}}': ['exact', 'lt', 'gt'], {% endfor %}
+        }
 
 
 class {{modelFields.entity_name[0]}}Serializer(serializers.ModelSerializer):
@@ -12,12 +13,11 @@ class {{modelFields.entity_name[0]}}Serializer(serializers.ModelSerializer):
         fields = ('pk', {% for var in modelFields.child_vars[1]%}'{{var}}', {% endfor %}{% for var in modelFields.child_vars[2]%}'{{var}}', {% endfor %})
 
 
-class {{modelFields.entity_name[0]}}ViewSet(viewsets.ModelViewSet)}:
+class {{modelFields.entity_name[0]}}ViewSet(ModelViewSet):
     queryset = {{modelFields.entity_name[0]}}.objects.all()
     permission_classes = [permissions.IsAuthenticated]
-    http_method_names = ['get', 'options', 'post', 'put']
     filter_class = {{modelFields.entity_name[0]}}Filter
     serializer_class = {{modelFields.entity_name[0]}}Serializer
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter,)
     search_fields = ({% for var in modelFields.child_vars[1]%}'@{{var}}', {% endfor %}{% for var in modelFields.child_vars[2]%}'={{var}}', {% endfor %})
     ordering_fields = '__all__'

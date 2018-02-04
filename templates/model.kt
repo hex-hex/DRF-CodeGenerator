77c30e93509da9a2ed17a1f20b0791f3dc66c5fb
hugh <xiao.he@cfgglobal.co.nz>
2018-02-04 21:37:46 +1300

    add date

--- templates/model.kt
+++ templates/model.kt
@@ -7,4 +7,6 @@ class {{modelFields.entity_name}} { {% for var in modelFields.child_vars[1]%}
     var {{modelFields.toLowerCamel(var[0])}}: Double? = null {% endfor %}{% for var in modelFields.child_vars[3]%}
     var {{modelFields.toLowerCamel(var[0])}}: {{var[1][0]}}? = null {% endfor %}{% for var in modelFields.child_vars[4]%}
     var {{modelFields.toLowerCamel(var[0])}}: {{var[1][0]}}? = null {% endfor %}
+    {% if modelFields.has_base() %}var date_create: String? = null
+    var date_update: String? =null{% endif %}
 }

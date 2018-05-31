092f350694309006123cf6f2e23b976d93828f82
hugh <xiao.he@cfgglobal.co.nz>
2018-06-01 00:35:05 +0800

    update packages

--- test.py
+++ test.py
@@ -3,26 +3,11 @@ from Structrues import EntityCode
 if __name__ == '__main__':
     code = """
 
-class OrderHeader(EntityBase):
-    ORDER_STATUS = (
-        (0, 'QUOTE'),
-        (1, 'DRAFT'),
-        (2, 'BUYER_CONFIRMED'),
-        (3, 'SELLER_CONFIRMED'),
-        (4, 'BOTH_CONFIRMED'),
-        (5, 'SHIPPING'),
-        (6, 'EXPIRED'),
-        (7, 'DELETED'),
-        (8, 'COMPLETED'),
-    )
-    order_status = models.IntegerField(choices=ORDER_STATUS, default=0)
-    order_number = models.CharField(max_length=16, null=True, blank=True)
-    order_from = models.ForeignKey('Company', related_name='order_from_company', null=True, blank=True)
-    order_to = models.ForeignKey('Company', related_name='order_to_company', null=True, blank=True)
-    bill_to = models.ForeignKey('Company', related_name='bill_to_company', null=True, blank=True)
-    pay_to = models.ForeignKey('Company', related_name='pay_to_company', null=True, blank=True)
-    date_order = models.DateField(null=True, blank=True)
-    description = models.CharField(max_length=1024, null=True, blank=True)
-    
+class ArticleTag(EntityBase):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 """
     code = EntityCode(code)
